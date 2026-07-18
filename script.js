6488c82aa3a6536d61774db873d57f7772190f4d
valerie-byte <vn1.thouvenot@gmail.com>
2026-07-18 11:59:33 +0200

    mise à jour de la galerie photo

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const galllery = document.getElementById("gallery");
 
-for (let i = 1; i<=29; i++) {
+for (let i = 1; i<=16; i++) {
     // ajoute un élément HTML
     galllery.innerHTML +=
-    `<div class="col-6 col-md-4 col-lg-3 ">
+    `<div class="col-12 col-sm-6 col-lg-4 ">
         <img src="img/gallery/img_gallery${i}.jpg" 
         class="img-fluid rounded shadow "
         alt="image_intervention_nuisibles${i}"></img>
